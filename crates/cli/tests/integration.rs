52ffddd821fb10ec41cf6e9e67712a7a6a0a6d81
Alex Ostrovski <ostrovski.alex@gmail.com>
2022-12-12 10:56:58 +0200

    Check readability of CLI errors

--- crates/cli/tests/integration.rs
+++ crates/cli/tests/integration.rs
@@ -1,6 +1,6 @@
 //! Integration tests for the externref CLI.
 
-#![cfg(unix)] // sh-specific input
+#![cfg(unix)] // sh-specific user inputs
 
 use term_transcript::{
     svg::{Template, TemplateOptions},
@@ -19,16 +19,33 @@ fn test_config() -> TestConfig<PtyCommand> {
     let shell_options = ShellOptions::new(PtyCommand::default())
         .with_cargo_path()
         .with_current_dir(env!("CARGO_MANIFEST_DIR"));
-    TestConfig::new(shell_options)
+    TestConfig::new(shell_options).with_template(template())
 }
 
 #[test]
-fn cli_basics() {
+fn cli_with_tracing() {
     // The WASM module is taken from the end-to-end test. We check it into the version control
     // in order for this test to be autonomous.
-    test_config().with_template(template()).test(
+    test_config().test(
         "tests/snapshots/with-tracing.svg",
         ["RUST_LOG=externref=info \\\n  \
             externref --drop-fn test::drop -o /dev/null tests/test.wasm"],
+    );
+}
+
+/// This and the following tests ensure that the error message is human-readable.
+#[test]
+fn error_processing_module() {
+    test_config().test(
+        "tests/snapshots/error-processing.svg",
+        ["externref --drop-fn test::drop -o /dev/null tests/integration.rs"],
+    );
+}
+
+#[test]
+fn error_specifying_drop_fn() {
+    test_config().test(
+        "tests/snapshots/error-drop-fn.svg",
+        ["externref --drop-fn test_drop -o /dev/null tests/test.wasm"],
     );
 }
