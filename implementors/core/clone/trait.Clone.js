6f9eafcd2c25942cdf13435811f623ba20827677
slowli <slowli@users.noreply.github.com>
2024-07-06 07:26:07 +0000

    Deploying to gh-pages from @ slowli/externref@4603c80cb31f675cefd5ce0312ba8b65feabbf1d 🚀

---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"externref":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"externref/struct.BitSlice.html\" title=\"struct externref::BitSlice\">BitSlice</a>&lt;'a&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
