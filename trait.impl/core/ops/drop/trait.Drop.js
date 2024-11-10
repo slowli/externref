1c5869aed3ab642bf77b28b571c5f2a324da0b20
slowli <slowli@users.noreply.github.com>
2024-11-10 15:41:05 +0000

    Deploying to gh-pages from @ slowli/externref@a08e072750fb6cdfea46a0ea06b6ad5a75b33584 🚀

---
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"externref":[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"externref/struct.Resource.html\" title=\"struct externref::Resource\">Resource</a>&lt;T&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
