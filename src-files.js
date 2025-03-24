71f9dba0c88a31a2f72ca94cfacee33c71d5f26e
slowli <slowli@users.noreply.github.com>
2025-03-24 20:46:15 +0000

    Deploying to gh-pages from @ slowli/externref@9c2d19a03ed1a747aefd4675be28bdad24c1a418 🚀

---
+++ src-files.js
@@ -0,0 +1,5 @@
+var srcIndex = new Map(JSON.parse('[\
+["externref",["",[["processor",[],["error.rs","functions.rs","mod.rs","state.rs"]]],["error.rs","lib.rs","signature.rs"]]],\
+["externref_macro",["",[],["externref.rs","lib.rs"]]]\
+]'));
+createSrcSidebar();
